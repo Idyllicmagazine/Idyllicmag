4cce23617332c0c588036bef9c7d7fc81a136236
Idyllicmagazine <88435068+Idyllicmagazine@users.noreply.github.com>
2021-08-04 17:00:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Datatypes in Javascript -
+// Primitive 
+// undefined 
+// Boolean
+// Number
+// String
+// BigInt
+// symbol 
+// null
+
+// var myName = "sanskriti" ; //string
+// var myAge = 15;           //number
+// var ageTrue = false;    //boolean
+// var not_Defined = null  //null
+// console.log(ageTrue)
+
+var num = 10;
+var newNum = ++num; 
+console.log(newNum)
